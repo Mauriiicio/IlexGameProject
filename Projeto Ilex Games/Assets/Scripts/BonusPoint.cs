4ed7584c0890ecf85a6a880681dc81ecda3b623a
Mauriiicio <mauricio1desouza@gmail.com>
2025-05-15 21:19:52 -0400

    alteracao pontuacao

--- Projeto Ilex Games/Assets/Scripts/BonusPoint.cs
+++ Projeto Ilex Games/Assets/Scripts/BonusPoint.cs
@@ -1,18 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEditor.Experimental.GraphView.GraphView;
 
-public class BonusPoint : MonoBehaviour
-{
+public class BonusPoint : MonoBehaviour {
     //coletavel que da pontos ao jogador.
     [SerializeField]
     private int points;
-    private void OnTriggerEnter2D(Collider2D collision)
-    {
+    private void OnTriggerEnter2D(Collider2D collision) {
         Player player = collision.GetComponent<Player>();
-        if(player != null)
-        {
+        if (player != null) {
             GameManager.gameManager.Score(points);
+            if (GameManager.gameManager.player1.IsDead() && !GameManager.gameManager.player2.IsDead()) {
+                GameManager.gameManager.player1.Revive();
+            } else if (GameManager.gameManager.player2.IsDead() && !GameManager.gameManager.player1.IsDead()) {
+                GameManager.gameManager.player2.Revive();
+            }
             Destroy(gameObject);
         }
     }
