16d84fd793848eb694e6625e74a65fa40665b112
Mauriiicio <mauricio1desouza@gmail.com>
2025-05-15 23:21:40 -0400

    atualizacao com os comandos do fliperama

--- Projeto Ilex Games/Assets/Scripts/BonusPoint.cs
+++ Projeto Ilex Games/Assets/Scripts/BonusPoint.cs
@@ -1,10 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using static UnityEditor.Experimental.GraphView.GraphView;
+
 
 public class BonusPoint : MonoBehaviour {
-    //coletavel que da pontos ao jogador.
     [SerializeField]
     private int points;
     private void OnTriggerEnter2D(Collider2D collision) {
